40ede004cf52b673620bac9f7d857fe7f6fe2032
antonio-f <anf@tuta.io>
2023-06-27 07:55:31 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 import pickle
+import dill
 
 from src.exception import CustomException
 
@@ -53,5 +54,17 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+    
+
     
 
